bd7f4747a5b894044aa293588fd1b22498aa4cde
njh27 <njh27@duke.edu>
2023-04-18 16:37:54 -0400

    add test function for precomputing bin lags

--- NeuronAnalysis/fit_CS_learning_fun.py
+++ NeuronAnalysis/fit_CS_learning_fun.py
@@ -278,7 +278,7 @@ class FitCSLearningFun(object):
         for plag in lags_pf:
             for mlag in lags_mli:
                 # if ( (plag % 5 == 0) and (mlag % 5 == 0) ):
-                if (mlag % quick_lag_step == 0):
+                if (mlag % 10 == 0):
                     print("current pf lag and mli lag:", plag, mlag)
 
 
@@ -350,9 +350,9 @@ class FitCSLearningFun(object):
             n_fit = 0
             for plag in lags_pf:
                 for mlag in lags_mli:
-                    if (mlag % quick_lag_step == 0):
+                    if (mlag % 4 == 0):
                         print("current pf lag and mli lag:", plag, mlag)
-                        
+
                     eye_data[:, :, 0:4] = self.get_eye_lag_slice(plag, eye_data_all_lags)
                     eye_data[:, :, 4:8] = self.get_eye_lag_slice(mlag, eye_data_all_lags)
                     # Use bin smoothing on data before fitting
@@ -612,7 +612,7 @@ class FitCSLearningFun(object):
         for plag in lags_pf:
             for mlag in lags_mli:
                 # if ( (plag % 5 == 0) and (mlag % 5 == 0) ):
-                if (mlag % quick_lag_step == 0):
+                if (mlag % 10 == 0):
                     print("current pf lag and mli lag:", plag, mlag)
 
 
@@ -692,7 +692,7 @@ class FitCSLearningFun(object):
             n_fit = 0
             for plag in lags_pf:
                 for mlag in lags_mli:
-                    if (mlag % quick_lag_step == 0):
+                    if (mlag % 4 == 0):
                         print("current pf lag and mli lag:", plag, mlag)
 
                     eye_data[:, :, 0:4] = binned_eye_data_by_lag[plag]
