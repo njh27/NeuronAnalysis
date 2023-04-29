5380b8c9678341b66db33a854b5f1b544647194f
njh27 <njh27@duke.edu>
2023-04-28 20:27:23 -0400

    adding inhibitory relus

--- NeuronAnalysis/fit_NN_model.py
+++ NeuronAnalysis/fit_NN_model.py
@@ -293,7 +293,7 @@ class FitNNModel(object):
                                                                             bin_eye_data_test[:, k],
                                                                             fixed_means,
                                                                             fixed_sigma)
-        for k in range(0)
+
             eye_input_train[:, (first_relu_ind + 2 * k)] = negative_relu(
                                                                 bin_eye_data_train[:, 4 + k],
                                                                 c=0.0)
