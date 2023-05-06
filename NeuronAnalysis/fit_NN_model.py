c7e6022cd0d8279a05bc1d784072d629af0306cd
njh27 <njh27@duke.edu>
2023-05-06 14:47:53 -0400

    added convolved CS for LTD kernel also

--- NeuronAnalysis/fit_NN_model.py
+++ NeuronAnalysis/fit_NN_model.py
@@ -1092,13 +1092,13 @@ def fit_learning_rates(NN_FIT, blocks, trial_sets, bin_width=10, bin_threshold=5
             # LTP_trial = np.mod(CS[trial*n_obs_pt:(trial + 1)*n_obs_pt] + 1, 2) # Opposite 1's and 0's as CS
             LTP_trial = postsynaptic_decay_FR(CS_trial, tau_rise=tau_rise,
                                 tau_decay=tau_decay, kernel_area=kernel_area,
-                                min_val=1.0)
+                                min_val=1.0, reverse=False)
             # LTP_trial = LTP_trial * y_obs_trial / 1000
             LTP_on_Inputs = np.dot(LTP_trial, state_input) # Sum of LTP over activation for each input unit
             LTP_bound = (W_max - W).squeeze()
             LTP_bound[LTP_bound < 1e-5] = 1e-5
             LTP_on_Inputs = LTP_on_Inputs * LTP_bound
-            # W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
+
             W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
 
             """ CS only learning with no LTP! """
@@ -1253,13 +1253,12 @@ def get_learning_weights_by_trial(NN_FIT, blocks, trial_sets, W_0=None,
         # LTP_trial = np.mod(CS[trial_ind*n_obs_pt:(trial_ind + 1)*n_obs_pt] + 1, 2) # Opposite 1's and 0's as CS
         LTP_trial = postsynaptic_decay_FR(CS_trial, tau_rise=tau_rise,
                             tau_decay=tau_decay, kernel_area=kernel_area,
-                            min_val=1.0)
+                            min_val=1.0, reverse=False)
         # LTP_trial = LTP_trial * y_obs_trial / 1000
         LTP_on_Inputs = np.dot(LTP_trial, state_input) # Sum of LTP over activation for each input unit
         LTP_bound = (W_max - W).squeeze()
         LTP_bound[LTP_bound < 1e-5] = 1e-5
         LTP_on_Inputs = LTP_on_Inputs * LTP_bound
-        # W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
         W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
 
         """ CS only learning with no LTP! """
