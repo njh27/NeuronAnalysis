68ce26a5ce67f3f1b5efdd0241f92ef745d0d334
njh27 <njh27@duke.edu>
2023-05-06 13:09:11 -0400

    added convolved CS for LTD kernel also

--- NeuronAnalysis/fit_NN_model.py
+++ NeuronAnalysis/fit_NN_model.py
@@ -1087,7 +1087,7 @@ def fit_learning_rates(NN_FIT, blocks, trial_sets, bin_width=10, bin_threshold=5
             # CS_trial = postsynaptic_decay_FR(CS_trial, tau_rise=tau_rise_CS,
             #                     tau_decay=tau_decay_CS, kernel_area=kernel_area_CS,
             #                     min_val=0.0, reverse=True)
-            CS_trial = CS_trial * y_obs_trial/1000
+            # CS_trial = CS_trial * y_obs_trial/1000
             CS_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
             CS_on_Inputs = CS_on_Inputs * W.squeeze()
 
@@ -1095,7 +1095,7 @@ def fit_learning_rates(NN_FIT, blocks, trial_sets, bin_width=10, bin_threshold=5
             LTP_trial = postsynaptic_decay_FR(CS_trial, tau_rise=tau_rise,
                                 tau_decay=tau_decay, kernel_area=kernel_area,
                                 min_val=1.0)
-            # LTP_trial = LTP_trial * y_obs_trial
+            LTP_trial = LTP_trial * y_obs_trial / 1000
             LTP_on_Inputs = np.dot(LTP_trial, state_input) # Sum of LTP over activation for each input unit
             LTP_bound = (W_max - W).squeeze()
             LTP_bound[LTP_bound < 1e-5] = 1e-5
@@ -1248,7 +1248,7 @@ def get_learning_weights_by_trial(NN_FIT, blocks, trial_sets, W_0=None,
         # CS_trial = postsynaptic_decay_FR(CS_trial, tau_rise=tau_rise_CS,
         #                     tau_decay=tau_decay_CS, kernel_area=kernel_area_CS,
         #                     min_val=0.0, reverse=True)
-        CS_trial = CS_trial * y_obs_trial / 1000
+        # CS_trial = CS_trial * y_obs_trial / 1000
         CS_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
         CS_on_Inputs = CS_on_Inputs * W.squeeze()
 
@@ -1256,7 +1256,7 @@ def get_learning_weights_by_trial(NN_FIT, blocks, trial_sets, W_0=None,
         LTP_trial = postsynaptic_decay_FR(CS_trial, tau_rise=tau_rise,
                             tau_decay=tau_decay, kernel_area=kernel_area,
                             min_val=1.0)
-        # LTP_trial = LTP_trial * y_obs_trial
+        LTP_trial = LTP_trial * y_obs_trial / 1000
         LTP_on_Inputs = np.dot(LTP_trial, state_input) # Sum of LTP over activation for each input unit
         LTP_bound = (W_max - W).squeeze()
         LTP_bound[LTP_bound < 1e-5] = 1e-5
