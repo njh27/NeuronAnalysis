6c45d7941979631589bf319a10b59c38905692e9
njh27 <njh27@duke.edu>
2023-05-05 20:01:05 -0400

    adding firing rate dependent to CS

--- NeuronAnalysis/fit_NN_model.py
+++ NeuronAnalysis/fit_NN_model.py
@@ -1063,12 +1063,13 @@ def fit_learning_rates(NN_FIT, blocks, trial_sets, bin_width=10, bin_threshold=5
             # Update weights for next trial based on activations in this trial
             state_input = state_input[:, 0:n_gaussians]
 
-            state_input = state_input * y_obs_trial[:, None]
             CS_trial = CS[trial*n_obs_pt:(trial + 1)*n_obs_pt] # CS for this trial
+            CS_trial = CS_trial * y_obs_trial[:, None]
             CS_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
 
 
             LTP_trial = np.mod(CS_on_Inputs + 1, 2) # Opposite 1's and 0's as CS
+            # LTP_trial = LTP_trial * y_obs_trial[:, None]
             LTP_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
             # W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
             W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
@@ -1189,12 +1190,14 @@ def get_learning_weights_by_trial(NN_FIT, blocks, trial_sets, W_0=None,
         # for these states are not affected during nans
         state_input[eye_is_nan_trial, :] = 0.0
         state_input = state_input[:, 0:n_gaussians]
-        state_input = state_input * y_obs_trial[:, None]
+        # state_input = state_input * y_obs_trial[:, None]
         # Update weights for next trial based on activations in this trial
         CS_trial = CS[trial_ind*n_obs_pt:(trial_ind + 1)*n_obs_pt] # CS for this trial
+        CS_trial = CS_trial * y_obs_trial[:, None]
         CS_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
 
         LTP_trial = np.mod(CS_on_Inputs + 1, 2) # Opposite 1's and 0's as CS
+        # LTP_trial = LTP_trial * y_obs_trial[:, None]
         LTP_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
         # W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
         W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
