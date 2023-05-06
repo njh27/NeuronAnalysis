c95ea8e4e570136e5f089d7d2f4b3f916834e1ef
njh27 <njh27@duke.edu>
2023-05-06 13:26:02 -0400

    added convolved CS for LTD kernel also

--- NeuronAnalysis/fit_NN_model.py
+++ NeuronAnalysis/fit_NN_model.py
@@ -1086,7 +1086,7 @@ def fit_learning_rates(NN_FIT, blocks, trial_sets, bin_width=10, bin_threshold=5
             CS_trial = postsynaptic_decay_FR(CS_trial, tau_rise=tau_rise_CS,
                                 tau_decay=tau_decay_CS, kernel_area=kernel_area_CS,
                                 min_val=0.0, reverse=True)
-            # CS_trial = CS_trial * y_obs_trial/1000
+            CS_trial = CS_trial * y_obs_trial/1000
             CS_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
             CS_on_Inputs = CS_on_Inputs * W.squeeze()
 
@@ -1248,7 +1248,7 @@ def get_learning_weights_by_trial(NN_FIT, blocks, trial_sets, W_0=None,
         CS_trial = postsynaptic_decay_FR(CS_trial, tau_rise=tau_rise_CS,
                             tau_decay=tau_decay_CS, kernel_area=kernel_area_CS,
                             min_val=0.0, reverse=True)
-        # CS_trial = CS_trial * y_obs_trial / 1000
+        CS_trial = CS_trial * y_obs_trial / 1000
         CS_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
         CS_on_Inputs = CS_on_Inputs * W.squeeze()
 
