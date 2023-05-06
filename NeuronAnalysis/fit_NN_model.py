4e568ebfc4da7990ef88a06cf7f4d1ac9f0fc529
njh27 <njh27@duke.edu>
2023-05-05 20:59:55 -0400

    adding weights changing as function of current weight

--- NeuronAnalysis/fit_NN_model.py
+++ NeuronAnalysis/fit_NN_model.py
@@ -1067,15 +1067,12 @@ def fit_learning_rates(NN_FIT, blocks, trial_sets, bin_width=10, bin_threshold=5
             CS_trial = CS[trial*n_obs_pt:(trial + 1)*n_obs_pt] # CS for this trial
             # CS_trial = CS_trial * y_obs_trial
             CS_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
-            CS_on_Inputs = CS_on_Inputs * W
+            CS_on_Inputs = CS_on_Inputs * W.squeeze()
 
             LTP_trial = np.mod(CS[trial*n_obs_pt:(trial + 1)*n_obs_pt] + 1, 2) # Opposite 1's and 0's as CS
             # LTP_trial = LTP_trial * y_obs_trial
             LTP_on_Inputs = np.dot(LTP_trial, state_input) # Sum of CS over activation for each input unit
-            print("first LTP", LTP_on_Inputs.shape)
-            LTP_on_Inputs = LTP_on_Inputs * (W_max - W)
-            print("second LTP", LTP_on_Inputs.shape)
-            print("with W's", W_max.shape, (W_max - W).shape)
+            LTP_on_Inputs = LTP_on_Inputs * (W_max - W).squeeze()
             # W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
             W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
 
@@ -1203,12 +1200,12 @@ def get_learning_weights_by_trial(NN_FIT, blocks, trial_sets, W_0=None,
         CS_trial = CS[trial_ind*n_obs_pt:(trial_ind + 1)*n_obs_pt] # CS for this trial
         # CS_trial = CS_trial * y_obs_trial
         CS_on_Inputs = np.dot(CS_trial, state_input) # Sum of CS over activation for each input unit
-        CS_on_Inputs = CS_on_Inputs * W
+        CS_on_Inputs = CS_on_Inputs * W.squeeze()
 
         LTP_trial = np.mod(CS[trial_ind*n_obs_pt:(trial_ind + 1)*n_obs_pt] + 1, 2) # Opposite 1's and 0's as CS
         # LTP_trial = LTP_trial * y_obs_trial
         LTP_on_Inputs = np.dot(LTP_trial, state_input) # Sum of CS over activation for each input unit
-        LTP_on_Inputs = LTP_on_Inputs * (W_max - W)
+        LTP_on_Inputs = LTP_on_Inputs * (W_max - W).squeeze()
         # W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
         W += ( alpha * LTP_on_Inputs[:, None] - beta * CS_on_Inputs[:, None] )
 
