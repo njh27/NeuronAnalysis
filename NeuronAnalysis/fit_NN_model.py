0ebcdef87d7ec86627d6eea987276663df16000a
njh27 <njh27@duke.edu>
2023-05-07 09:28:42 -0400

    Fixed bug where LTP was convolved with CS kernel instead of binary

--- NeuronAnalysis/fit_NN_model.py
+++ NeuronAnalysis/fit_NN_model.py
@@ -964,8 +964,8 @@ CS_decay_kernel = False
 CS_rates = False
 CS_weights = True
 
-LTP_decay_kernel = True
-LTP_rates = False
+LTP_decay_kernel = False
+LTP_rates = True
 LTP_weights = True
 
 def fit_learning_rates(NN_FIT, blocks, trial_sets, bin_width=10, bin_threshold=5):
