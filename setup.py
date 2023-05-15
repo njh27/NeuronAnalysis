0bc13cc74514031876a8a6537fb41efdd4c625e8
njh27 <njh27@duke.edu>
2023-05-15 15:20:29 -0400

    added compile to outer setup for install

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
 import numpy as np
 
 
-
+ext  =  [Extension( "fit_learning_rates", sources=['fit_learning_rates.pyx'] )]
 setup(name='NeuronAnalysis',
       version='1.0',
       description='Basic timeseries organization of neural data.',
@@ -10,5 +12,6 @@ setup(name='NeuronAnalysis',
       author_email='nathan.hall@duke.edu',
       url='https://',
       packages=['NeuronAnalysis'],
+      ext_modules = cythonize(ext, annotate=False, language_level=3),
       include_dirs=[np.get_include()],
      )
