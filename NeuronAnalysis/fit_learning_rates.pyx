eeb035378c7dca008ee0e25521d7fe41f633dbfc
njh27 <njh27@duke.edu>
2023-05-16 06:20:11 -0400

    updated inputs for views vs. arrays

--- NeuronAnalysis/fit_learning_rates.pyx
+++ NeuronAnalysis/fit_learning_rates.pyx
@@ -186,7 +186,7 @@ cdef void update_W_pf(np.ndarray[double, ndim=1] W_pf,
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef double learning_function(np.ndarray[double, ndim=1] params,
+cdef double[:, :] learning_function(np.ndarray[double, ndim=1] params,
                                                   np.ndarray[double, ndim=2] x,
                                                   np.ndarray[double, ndim=1] y,
                                                   np.ndarray[double, ndim=1] W_0_pf,
