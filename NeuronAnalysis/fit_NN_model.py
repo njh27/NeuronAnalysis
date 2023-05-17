887f1e864a3425f84bac64e285f586201e825d9c
njh27 <njh27@duke.edu>
2023-05-17 19:36:59 -0400

    expanded move weights independently

--- NeuronAnalysis/fit_NN_model.py
+++ NeuronAnalysis/fit_NN_model.py
@@ -981,9 +981,9 @@ def learning_function(params, x, y, W_0_pf, W_0_mli, b, *args, **kwargs):
         zeta_f_move = np.sqrt(move_m_trial) * move_LTP_scale
         # Create the LTP function for parallel fibers
         pf_LTP_funs = f_pf_CS_LTP(CS_trial_bin, kwargs['tau_rise_CS_LTP'],
-                        kwargs['tau_decay_CS_LTP'], alpha, zeta_f_move)
+                        kwargs['tau_decay_CS_LTP'], alpha, zeta_f_move=None)
         # These functions add on to pf_LTP_funs in place
-        pf_LTP_funs = f_pf_FR_LTP(pf_LTP_funs, y_obs_trial, beta, zeta_f_move)
+        pf_LTP_funs = f_pf_FR_LTP(pf_LTP_funs, y_obs_trial, beta, zeta_f_move=None)
         pf_LTP_funs = f_pf_static_LTP(pf_LTP_funs, pf_CS_LTD, gamma, zeta_f_move)
         # pf_LTP_funs = f_pf_move_LTP(pf_LTP_funs, move_m_trial, move_LTP_scale)
         # Convert to LTP input for Purkinje cell
@@ -1324,9 +1324,9 @@ def get_learning_weights_by_trial(NN_FIT, blocks, trial_sets, W_0_pf=None,
         zeta_f_move = np.sqrt(move_m_trial) * move_LTP_scale
         # Create the LTP function for parallel fibers
         pf_LTP_funs = f_pf_CS_LTP(CS_trial_bin, kwargs['tau_rise_CS_LTP'],
-                        kwargs['tau_decay_CS_LTP'], alpha, zeta_f_move)
+                        kwargs['tau_decay_CS_LTP'], alpha, zeta_f_move=None)
         # These functions add on to pf_LTP_funs in place
-        pf_LTP_funs = f_pf_FR_LTP(pf_LTP_funs, y_obs_trial, beta, zeta_f_move)
+        pf_LTP_funs = f_pf_FR_LTP(pf_LTP_funs, y_obs_trial, beta, zeta_f_move=None)
         pf_LTP_funs = f_pf_static_LTP(pf_LTP_funs, pf_CS_LTD, gamma, zeta_f_move)
         # pf_LTP_funs = f_pf_move_LTP(pf_LTP_funs, move_m_trial, move_LTP_scale)
         # Convert to LTP input for Purkinje cell
