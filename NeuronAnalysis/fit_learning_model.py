c638fed49cb36b3992c4a636b0342ec062011bb9
njh27 <njh27@duke.edu>
2023-05-18 20:04:50 -0400

    loop over CS wins seems working

--- NeuronAnalysis/fit_learning_model.py
+++ NeuronAnalysis/fit_learning_model.py
@@ -847,7 +847,7 @@ def get_intrisic_rate_and_CSwin(NN_FIT, blocks, trial_sets, learn_fit_window=Non
     #             [[50, 0],     [-100, 200]],
     #             [[0, 50],     [-150, 250]],
                 # ]
-    test_intrinsic_rates = [x for x in np.linspace(0, median_FR, 1)]
+    test_intrinsic_rates = [x for x in np.linspace(0, median_FR, 2)]
     test_intrinsic_rates[0] = None
     CS_wins = [ [[100, -50],  [-50, 150]],
                 [[50, 0],     [-100, 200]],
