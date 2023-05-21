6e3deb2973f7154de123b4176e648e5a758eba93
njh27 <njh27@duke.edu>
2023-05-21 14:17:31 -0400

    Prediction functions updated

--- NeuronAnalysis/fit_learning_model.py
+++ NeuronAnalysis/fit_learning_model.py
@@ -37,11 +37,12 @@ def comp_learning_response(NN_FIT, X_trial, W_trial, return_comp=False):
     pf_in = np.zeros((X_trial.shape[0], X_trial.shape[1]))
     mli_in = np.zeros((X_trial.shape[0], X_trial.shape[1]))
 
+    X_input = np.zeros((W_trial.shape[1], ))
     for t_ind in range(0, X_trial.shape[0]):
         # Transform X_data for this trial into input space
         X_input = eye_input_to_PC_gauss_relu(X_trial[t_ind, :, :],
                                         gauss_means, gauss_stds,
-                                        n_gaussians_per_dim=n_gaussians_per_dim)
+                                        n_gaussians_per_dim, X_input)
         # Each trial update the weights for W
         W[:, 0] = W_trial[t_ind, :]
         y_hat[t_ind, :] = (np.dot(X_input, W) + b).squeeze()
